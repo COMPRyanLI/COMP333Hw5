04c2a7d1bc8b5330062bd0956953ecea9abf02f2
COMPRyanLI <rli03@wesleyan.edu>
2023-12-05 18:02:35 -0500

    Now all bugs are fixed

--- App.test.js
+++ App.test.js
@@ -1,47 +1,118 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import App from './App';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import axios from 'axios';
+import App from './App';
 
+jest.mock('axios');
 
-describe('Login and Registration Functionality', () => {
-  it('renders login form with input fields and buttons', () => {
-    render(<App />);
-    
-    // Check if login form is rendered with input fields and buttons
-    expect(screen.getByText('Song Rating App')).toBeInTheDocument();
-    expect(screen.getByText('Login')).toBeInTheDocument();
-    expect(screen.getByLabelText('Username:')).toBeInTheDocument();
-    expect(screen.getByLabelText('Password:')).toBeInTheDocument();
-    expect(screen.getByText('Login')).toBeInTheDocument();
-    expect(screen.getByText('Register')).toBeInTheDocument();
+describe('User Login and Registration', () => {
+  beforeEach(() => {
+    // we need to setup mock axios
+    axios.get.mockResolvedValue({ data: [] }); // a function provided by Jest when you mock a module
+    axios.post.mockResolvedValue({ status: 200, data: true });
   });
 
-  it('navigates from login page to registration page', () => {
+  test('renders login form and interacts with it', async () => {
     render(<App />);
-    
-    // Click the 'Register' button to navigate to registration page
-    fireEvent.click(screen.getByText('Register'));
-    
-    // Check if the registration form is rendered after clicking 'Register'
-    expect(screen.getByText('Register')).toBeInTheDocument();
+    // check if the login form is rendered
+    expect(screen.getByRole('heading', { name: 'Login' })).toBeInTheDocument(); 
     expect(screen.getByLabelText('Username:')).toBeInTheDocument();
     expect(screen.getByLabelText('Password:')).toBeInTheDocument();
-    expect(screen.getByText('Register')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Login' })).toBeInTheDocument();
+  
+    // Simulate user input
+    fireEvent.change(screen.getByLabelText('Username:'), { target: { value: 'username' } });
+    fireEvent.change(screen.getByLabelText('Password:'), { target: { value: 'password' } });
+  
+    // Simulate form submission
+    fireEvent.click(screen.getByRole('button', { name: 'Login' }));
+
+    // Expect the login function to be called
+    await waitFor(() => {
+      expect(axios.post).toHaveBeenCalledWith(expect.anything(), { username: 'username', password: 'password' });
+    });
   });
 
-  it('simulates user input and registration with incorrect value', () => {
+  test('renders registration form when register button is clicked', async () => {
     render(<App />);
-    
-    // Click the 'Register' button to navigate to registration page
-    fireEvent.click(screen.getByText('Register'));
+  // Click the Register button to switch to the registration form
+  fireEvent.click(screen.getByRole('button', { name: 'Register' }));
 
-    // Fill in the registration form with incorrect values and submit
+  // Verify registration form is rendered
+  expect(screen.getByRole('heading', { name: 'Register' })).toBeInTheDocument();
+  expect(screen.getByLabelText('Username:')).toBeInTheDocument();
+  expect(screen.getByLabelText('Password:')).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: 'Register' })).toBeInTheDocument();
+
+  // Simulate user input for registration
+  fireEvent.change(screen.getByLabelText('Username:'), { target: { value: 'newuser' } });
+  fireEvent.change(screen.getByLabelText('Password:'), { target: { value: 'newpassword' } });
+
+  // Simulate form submission for registration
+  fireEvent.click(screen.getByRole('button', { name: 'Register' }));
+
+    // Expect the registration function to be called
+    await waitFor(() => {
+      expect(axios.post).toHaveBeenCalledWith(expect.anything(), { username: 'newuser', password: 'newpassword' });
+    });
+  });
+
+  test('navigates from login to registration page', () => {
+    render(<App />);
+    // Assume clicking 'Register' navigates to the registration form
+    fireEvent.click(screen.getByRole('button', { name: 'Register' }));
+    expect(screen.getByRole('heading', { name: 'Register' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Register' })).toBeInTheDocument();
+  });
+
+  test('simulates typing into the username and password input boxes', () => {
+    render(<App />);
     fireEvent.change(screen.getByLabelText('Username:'), { target: { value: 'testuser' } });
-    fireEvent.change(screen.getByLabelText('Password:'), { target: { value: 'short' } });
-    fireEvent.click(screen.getByText('Register'));
+    fireEvent.change(screen.getByLabelText('Password:'), { target: { value: 'password123' } });
 
-    // Check if the error message is displayed for incorrect registration input
-    expect(screen.getByText('Please provide a valid username and a password with more than 8 characters.')).toBeInTheDocument();
+    expect(screen.getByLabelText('Username:').value).toBe('testuser');
+    expect(screen.getByLabelText('Password:').value).toBe('password123');
   });
+
+  test('detects an incorrect username input value', async () => {
+    axios.post.mockResolvedValueOnce({ status: 200, data: false }); // Mock a failed login
+    render(<App />);
+    fireEvent.change(screen.getByLabelText('Username:'), { target: { value: 'wrong' } });
+    fireEvent.change(screen.getByLabelText('Password:'), { target: { value: 'password' } });
+    fireEvent.click(screen.getByRole('button', { name: 'Login' }));
+
+    await waitFor(() => {
+      // Check for an error message after a failed login attempt
+      expect(screen.getByText(/invalid username or password/i)).toBeInTheDocument();
+    });
+  });
+
+  test('detects password too short during registration', async () => {
+    axios.post.mockResolvedValueOnce({ status: 200, data: false }); // Assert registration failed
+    render(<App />);
+    
+    // display the registration form
+    const registerButton = screen.getByRole('button', { name: 'Register' });
+    fireEvent.click(registerButton);
+    
+    // Simulate user input for registration with short password that is less than 10
+    const usernameInput = screen.getByLabelText('Username:');
+    const passwordInput = screen.getByLabelText('Password:');
+    fireEvent.change(usernameInput, { target: { value: 'username' } });
+    fireEvent.change(passwordInput, { target: { value: 's' } }); // let password be shorter than 10
+    
+    // Simulate form submission
+    const submitButton = screen.getByRole('button', { name: 'Register' });
+    fireEvent.click(submitButton);
+    
+    // Await and assert that the error message is displayed
+    // Replace '/password is too short/' with the actual error message text expected
+    await waitFor(() => {
+      expect(screen.getByText(/Please provide a valid username and a password with more than 10 characters./i)).toBeInTheDocument();
+    });
+  });
+  
+
+ 
 });
